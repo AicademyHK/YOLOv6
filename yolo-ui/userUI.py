130cc390844b32a2d3c9cad481425a4a952455e2
Ho Chun Fai <80579169+TheCoder0215@users.noreply.github.com>
2024-04-26 04:35:46 +0800

    added yolo-ui

---
+++ yolo-ui/userUI.py
@@ -0,0 +1,51 @@
+import gradio as gr
+from onnxbackend import load_model, update_labels, process_image, load_labels
+import os
+from PIL import Image
+
+def webcam_inference(image):
+    if model is None:
+        return image
+    pil_image = Image.fromarray(image)
+    result_text, processed_image = process_image(pil_image)
+    return processed_image
+
+def create_interface():
+    labels = load_labels(os.path.join(os.getcwd(), "data/id_labels.ini"))
+
+    with gr.Blocks() as demo:
+        with gr.Tab("Model & Inference"):
+            with gr.Row():
+                file_input = gr.File(label="Select ONNX Model", file_types=["onnx"], height=100)
+                width_input = gr.Textbox(label="Input Width", placeholder="640", type="text", value="640")
+                height_input = gr.Textbox(label="Input Height", placeholder="640", type="text", value="640")
+                provider_dropdown = gr.Dropdown(label="Select Execution Provider", choices=["CPU", "CUDA", "TensorRT"])
+                load_button = gr.Button("Load Model")
+            
+            with gr.Row():
+                label_inputs1 = [gr.Textbox(value=labels[i], label=f"Class {i}") for i in range(5)]
+            with gr.Row():
+                label_inputs2 = [gr.Textbox(value=labels[i+5], label=f"Class {i+5}") for i in range(5)]
+            update_button = gr.Button("Update Labels")
+            
+            with gr.Row():
+                image_input = gr.Image(sources=["upload"], label="Upload Image")
+                image_output = gr.Image(label="Processed Image")
+                infer_button = gr.Button("Run Inference")
+
+            # Set up interactions
+            load_button.click(
+                lambda file, width, height, provider: gr.update(value=load_model(file, width, height, provider)),
+                inputs=[file_input, width_input, height_input, provider_dropdown],
+                outputs=[]
+            )
+            update_button.click(lambda *args: gr.update(value=update_labels(*args)), inputs=label_inputs1+label_inputs2, outputs=[])
+            infer_button.click(process_image, inputs=image_input, outputs=image_output)
+        
+        with gr.Tab("Real-Time Detection"):
+            with gr.Row():
+                webcam_input = gr.Image(sources=["webcam"], label="Webcam Input")
+                webcam_output = gr.Image(label="Webcam Output")
+            webcam_input.change(webcam_inference, inputs=webcam_input, outputs=webcam_output)
+
+    return demo
